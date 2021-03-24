38b5b867818198ab100cb41ac3ec5cad39719563
qxxxb <46016483+qxxxb@users.noreply.github.com>
2021-03-24 02:53:20 -0400

    Add windows support

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
+#include <iostream>
 #include "SDL.h"
 
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
+    std::cout << "here" << std::endl;
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
 
     SDL_Window *window = SDL_CreateWindow(
         "SDL2Test",
